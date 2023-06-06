a40f48956b6df616b2d72f4de487ce48c4b6a2b1
gpsanant <gpsanant@gmail.com>
2023-06-05 19:05:32 -0700

    print generated

--- llama/inference.py
+++ llama/inference.py
@@ -7,10 +7,14 @@ MODEL_PATH="/gscratch/scrubbed/ebdaniel/llama/models/baseline/model_epoch_14.pt"
 tokenizer = llama.Tokenizer(model_path=TOKENIZER_PATH)
 model = llama.Transformer(model_path=MODEL_PATH)
 
+print("Loaded model")
+
 if torch.cuda.is_available():
     model = model.cuda()
 
 generator = llama.LLaMA(model, tokenizer)
+
+print("Loaded generator")
 
 empty_prompt_str = tokenizer.decode([tokenizer.bos_id])
 
@@ -18,4 +22,10 @@ prompts = [empty_prompt_str, "def bar(x):", "def baz(x):"]
 
 generated = generator.generate(prompts, max_gen_len=100)
 
+print("Generated")
+
+for prompt, gen in zip(prompts, generated):
+    print("Prompt:", prompt)
+    print("Generated:", gen)
+    print()
 
